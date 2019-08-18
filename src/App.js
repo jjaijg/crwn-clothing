5e0eefc0571ad8607e6bbd15d51c23a6496dd34a
jjaijg <jjaiganeshjg95@gmail.com>
2019-08-18 22:43:46 +0530

    Added route to project, created Shop page and Collection preview component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import logo from './logo.svg';
 import './App.css';
 
 import Homepage from './pages/homepage/Homepage.component';
+import ShopPage from './pages/shop/Shop.component';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Crown clothing </h1>
-      <Homepage />
+    <div>
+    <Switch>
+      <Route exact path='/' component={Homepage} />
+      <Route exact path='/shop' component={ShopPage} />
+    </Switch>
     </div>
   );
 }
