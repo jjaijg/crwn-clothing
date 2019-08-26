e2c925a71d73a10cf58a86b9d28ad2896e36401c
jjaijg <jjaiganeshjg95@gmail.com>
2019-08-26 07:47:45 +0530

    updating our app with selector, replaced cart selector with memoize selector

--- src/components/cart-dropdown/Cart-dropdown.component.jsx
+++ src/components/cart-dropdown/Cart-dropdown.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 
 import "./Cart-dropdown.style.scss";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 import CartItem from "../cart-item/Cart-item.component";
 import CustomButton from "../custom-button/Custom-button.component";
 
@@ -16,8 +17,8 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state)
 });
 
 export default connect(mapStateToProps)(CartDropdown);
